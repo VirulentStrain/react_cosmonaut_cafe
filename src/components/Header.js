9e1dd2c466add14f0a327621210ec5b62ceb0921
Alex Morgun <aamorgun2003@gmail.com>
2023-03-28 15:36:26 -0600

    Completed Header component; added a navtoggler button for certain viewports; I'll add functionality next

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,36 @@
-import { Navbar, NavItem, Collapse, NavbarToggler, Nav, NavLink, NavbarBrand, NavbarText } from 'reactstrap'; 
-import logo from '../assets/logo.png';
+import { Navbar, NavItem, Collapse, NavbarToggler, Nav, NavLink, NavbarBrand} from 'reactstrap'; 
 import { useState } from 'react';
+//import { NavLink } from 'react-router-dom';
+import logo  from '../assets/logo.png'
 import '../../src/App.css'
 
 
 const Header = () => {
 
-    const [navbarOpen, setNavbarOpen] = useState(false)
+    const [navOpen, setNavbarOpen] = useState(false)
 
     return (
-    
-       <Navbar color='light' role={navigator}>
-       {/* <NavbarBrand>
-            <img src={logo} width='100px'/>
-            <h1>Cosmonaut afe</h1>
-        </NavbarBrand> */}
-        {/* <NavbarToggler onClick={() => setNavbarOpen(!navbarOpen)}> */}
-            {/* <Collapse isOpen={navbarOpen} navbar> */}
-                <Nav navbar className='me-auto flex-row'>
-                    <NavItem  className='m-1'>
-                        <NavLink className='text-dark '>About</NavLink>
+    <>
+       <Navbar color='light' light role={navigator} expand='md'>
+        <NavbarBrand className='p-0'>
+            <img src={logo} width='400px' alt='website logo: cosmonaut cafe' className='float-start'/>
+        </NavbarBrand> 
+        <NavbarToggler className='ms-auto' onClick={() => setNavbarOpen(!navOpen)}/> 
+            <Collapse isOpen={navOpen} navbar> 
+                <Nav navbar className='ms-auto'>
+                    <NavItem>
+                        <NavLink className='text-dark'>About</NavLink>
                     </NavItem>
-                    <NavItem className='m-1'>
-                        <NavLink className='link-primary'>Inventory</NavLink>
+                    <NavItem>
+                        <NavLink className='text-dark'>Inventory</NavLink>
                     </NavItem>
-                    <NavItem className='m-1'>
+                    <NavItem>
                         <NavLink className='text-dark'>Login</NavLink>
                     </NavItem>
                 </Nav>
-            {/* </Collapse> */}
-        {/* </NavbarToggler> */}
+            </Collapse> 
        </Navbar>
-    
+    </>
     )
 }
 
